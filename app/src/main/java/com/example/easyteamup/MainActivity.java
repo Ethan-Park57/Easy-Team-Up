6dbdeeb39e5222048873447ce9cc821aecde50cc
bruckhausa <bruckhau@usc.edu>
2022-03-09 20:29:34 -0800

    add firebase instance

--- app/src/main/java/com/example/easyteamup/MainActivity.java
+++ app/src/main/java/com/example/easyteamup/MainActivity.java
@@ -11,10 +11,12 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.easyteamup.databinding.ActivityMainBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
